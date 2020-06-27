11332722cfec1eb48f14c23eb6b3ca4ead73a91c
PhuongTranThi <54054648+PhuongTranThi@users.noreply.github.com>
2020-06-27 12:29:39 +0700

    select schedule and render chải in page book

--- Website_BanVeXe/Controllers/BookController.cs
+++ Website_BanVeXe/Controllers/BookController.cs
@@ -29,29 +29,75 @@ namespace Website_BanVeXe.Controllers
             var ndi = form["noidi"];
             var nden = form["noiden"];
             var ngaykhoihanh = form["dateStart"];
+            var giokhoihanh = form["giokhoihanh"];
 
-            
+
             ViewData["noidi"] = ndi;
             ViewData["noiden"] = nden;
             ViewData["dateStart"] = ngaykhoihanh;
 
             //ViewData["chuyendi"] = bus_Ghe.LoadChuyenDi(int.Parse(ndi.ToString()), int.Parse(nden.ToString()), ngaykhoihanh);
-            var chuyenxedi = bus_Ghe.LoadTuyenDi(int.Parse(ndi.ToString()), int.Parse(nden.ToString()));
-            ViewData["tuyendi"] = chuyenxedi;
+            var tuyendi = bus_Ghe.LoadTuyenDi(int.Parse(ndi.ToString()), int.Parse(nden.ToString()));
+            ViewData["tuyendi"] = tuyendi;
 
-            ViewData["diadiem"] = bus_Ghe.LoadDiaDiemDon(chuyenxedi[0].ID_Tuyen);
-            
+            ViewData["diadiem"] = bus_Ghe.LoadDiaDiemDon(tuyendi[0].ID_Tuyen);
+            if (ngaykhoihanh != null) {
+                var chuyendi = bus_Ghe.LoadChuyenDi(tuyendi[0].ID_Tuyen, ngaykhoihanh);
+                ViewData["xe"] = bus_Ghe.Load_Ghe_Xe(chuyendi[0].BienSo);
+                ViewData["giokhoihanh"] = chuyendi;
+
+                //ViewData["chuyendi"] = bus_chuyen.LoadChuyenDi();
+                if (giokhoihanh != null) {
+                    ViewData["xe"] = bus_Ghe.Load_Ghe_Xe(giokhoihanh);
+                }
+                else {
+                    ViewData["xe"] = bus_Ghe.Load_Ghe_Xe(chuyendi[0].BienSo);
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult ViewChair(FormCollection form)
+        {
+            BUS_GHE bus_Ghe = new BUS_GHE();
+            BUS_ChuyenDi bus_chuyen = new BUS_ChuyenDi();
+
+
+
+            var ndi = form["noidi"];
+            var nden = form["noiden"];
+            var ngaykhoihanh = form["dateStart"];
+            var giokhoihanh = form["giokhoihanh"];
+
+
+            ViewData["noidi"] = ndi;
+            ViewData["noiden"] = nden;
+            ViewData["dateStart"] = ngaykhoihanh;
+
+            //ViewData["chuyendi"] = bus_Ghe.LoadChuyenDi(int.Parse(ndi.ToString()), int.Parse(nden.ToString()), ngaykhoihanh);
+            var tuyendi = bus_Ghe.LoadTuyenDi(int.Parse(ndi.ToString()), int.Parse(nden.ToString()));
+            ViewData["tuyendi"] = tuyendi;
+
+            ViewData["diadiem"] = bus_Ghe.LoadDiaDiemDon(tuyendi[0].ID_Tuyen);
             if (ngaykhoihanh != null)
             {
-                ViewData["giokhoihanh"] = bus_Ghe.LoadChuyenDi(chuyenxedi[0].ID_Tuyen, ngaykhoihanh);
+                var chuyendi = bus_Ghe.LoadChuyenDi(tuyendi[0].ID_Tuyen, ngaykhoihanh);
+                ViewData["xe"] = bus_Ghe.Load_Ghe_Xe(chuyendi[0].BienSo);
+                ViewData["giokhoihanh"] = chuyendi;
+
+                //ViewData["chuyendi"] = bus_chuyen.LoadChuyenDi();
+                if (giokhoihanh != null)
+                {
+                    ViewData["xe"] = bus_Ghe.Load_Ghe_Xe(giokhoihanh);
+                }
+                else
+                {
+                    ViewData["xe"] = bus_Ghe.Load_Ghe_Xe(chuyendi[0].BienSo);
+                }
             }
-            //ViewData["chuyendi"] = bus_chuyen.LoadChuyenDi();
-
-            ViewData["xe"] = bus_Ghe.Load_Ghe_Xe("55A-31589");
-
             return View();
         }
 
-      
+
     }
 }
